14d02386e478e925053f6f3bc1b62f8b1b0a48bd
Noah2211 <noah.decroix3@gmail.com>
2021-11-16 16:37:11 +0100

    remake exercice

--- app.js
+++ app.js
@@ -1,32 +1,21 @@
 let operande = 10;
+let entier = document.getElementById("entier");
+entier.innerHTML = parseInt(entier.innerHTML) * operande;
 
-let entier = "32";
-let resultEntier;
+let flottant = document.getElementById("flottant");
+flottant.innerHTML = parseFloat(flottant.innerHTML) * operande + parseInt(entier.innerHTML);
 
-let flottant = "54.85";
-let resultFlottant;
+let nonNumerique = document.getElementById("non-numerique");
 
-let nonNumerique = "Vous y arriverez";
-let resultNonNumerique;
-
-resultEntier = document.getElementById("entier").innerHTML = Number.parseInt(entier) * operande;
-resultFlottant = document.getElementById("flottant").innerHTML = Number.parseFloat(flottant) * operande + 320;
-
-if (isNaN(nonNumerique)) {
-    resultNonNumerique = document.getElementById("non-numerique").innerHTML = "0";
+if (isNaN(nonNumerique)){
+    nonNumerique.innerHTML = "0"
 }
 
-let random = document.getElementById("random").innerHTML = Math.random();
+let alea = document.getElementById("random");
+alea.innerHTML = Math.random();
 
-let trunc = "54.988";
-let resultTrunc;
+let trunc = document.getElementById("trunc");
+trunc.innerHTML = Math.trunc(trunc.innerHTML);
 
-resultTrunc = document.getElementById("trunc").innerHTML = Math.trunc(trunc);
-
-let replace = "Hello World !"
-let resultReplace;
-resultReplace = document.getElementById("replace").innerHTML = replace.replace("Hello World !", "Decroix Noah");
-
-
-
-
+let replace = document.getElementById("replace");
+replace.innerHTML = replace.innerHTML.replace("world", "Noah Decroix");
