fb18ed506cf4df69f026d2b8b9ba6cdacea64fab
Noah2211 <noah.decroix3@gmail.com>
2021-11-16 13:43:47 +0100

    exercice done

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-console.log(random());
+let operande = 10;
+
+let entier = "32";
+let resultEntier;
+
+let flottant = "54.85";
+let resultFlottant;
+
+let nonNumerique = "Vous y arriverez";
+let resultNonNumerique;
+
+resultEntier = document.getElementById("entier").innerHTML = Number.parseInt(entier) * operande;
+resultFlottant = document.getElementById("flottant").innerHTML = Number.parseFloat(flottant) * operande + 320;
+
+if (isNaN(nonNumerique)) {
+    resultNonNumerique = document.getElementById("non-numerique").innerHTML = "0";
+}
+
+let random = document.getElementById("random").innerHTML = Math.random();
+
+let trunc = "54.988";
+let resultTrunc;
+
+resultTrunc = document.getElementById("trunc").innerHTML = Math.trunc(trunc);
+
+let replace = "Hello World !"
+let resultReplace;
+resultReplace = document.getElementById("replace").innerHTML = replace.replace("Hello World !", "Decroix Noah");
+
+
+
+
